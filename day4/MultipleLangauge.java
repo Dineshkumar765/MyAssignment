541caa38a649f45f53b8eee4091926970dc3f110
Dineshkumar765 <108537336+Dineshkumar765@users.noreply.github.com>
2023-08-10 21:03:20 +0530

    Add files via upload
    week4day4

---
+++ day4/MultipleLangauge.java
@@ -0,0 +1,9 @@
+package week4.day4;
+
+public abstract class MultipleLangauge implements Language,TestTool {
+	public void python() {
+		System.out.println("python");
+	}
+	public abstract void ruby();
+
+}
